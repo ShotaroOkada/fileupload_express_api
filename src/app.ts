662ece17eed5f53b181005b7fcdc74b04146d8ce
ShotaroOkada <b1016117@gmail.com>
2019-10-13 21:26:23 +0900

    dbからgetしてくるまで

--- src/app.ts
+++ src/app.ts
@@ -1,11 +1,27 @@
-import * as Express from 'express';
+import Express from 'express';
+import * as admin from 'firebase-admin'
 import hogeController from './controller/hogeController';
 
 const app = Express();
 const portNumber = 3000;
 
+admin.initializeApp({
+    credential: admin.credential.applicationDefault(),
+    databaseURL: 'https://react-upload-file.firebaseio.com/'
+})
+
+const db = admin.database();
+const ref = db.ref();
+
+
 app.get('/', (req, res) => {
     res.send('hello world')
+    ref.on("value", response => {
+    console.log(response.val());
+    }, 
+    function(errorObject) {
+    console.log("The read failed: " + errorObject.code);
+    });
 });
 
 app.get('/hoge', hogeController)
