1af9aadb906219fcc6c2aa5ce00992472a51ce78
ShotaroOkada <b1016117@gmail.com>
2019-10-20 19:10:55 +0900

    storageに保存した画像のURLをdatabaseに保存させるところまで

--- src/controller/storage/postStorageController.ts
+++ src/controller/storage/postStorageController.ts
@@ -1,15 +1,33 @@
 import { Request, Response, NextFunction } from "express";
-import { bucket } from "../../firebase";
+import { bucket, db } from "../../firebase";
 
+// storageに画像をアップロードする
 export function postStorageController(req: Request, res: Response, next: NextFunction) {
     const path = '/Users/ShotaroOkada/Downloads/images.jpeg';
-    const filename = 'testImage'
-    bucket.upload(path, {destination:`test/${filename}`}, error => {
+    const uploadFilePath = 'test/testImage'
+    bucket.upload(path, {destination: uploadFilePath}, error => {
         if(error) {
             console.log('failed storage post');
             console.log(error.message);
         } else {
             console.log('success storage post');
+
+            // storageに画像をアップロードできた場合は、そのURLをrealtime databaseに保存する
+            const STORAGE_ROOT = "https://firebasestorage.googleapis.com/v0/b";
+            const bucketName = bucket.name;
+            const dlPath = encodeURIComponent(uploadFilePath);
+            const dlURL = `${STORAGE_ROOT}/${bucketName}/o/${dlPath}?alt=media`;
+
+            console.log(dlURL);
+
+            db.ref('files').push().set(dlURL , error => {
+                if(error) {
+                    console.log('failed database fileURL post');
+                } else {
+                    console.log('success database fileURL post');
+                }    
+            }
+            )
         }    
     })
 }
