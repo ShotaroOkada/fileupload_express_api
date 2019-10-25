739865298090eaf443daa7bb0e5a98dcbc5e07d5
ShotaroOkada <b1016117@gmail.com>
2019-10-25 16:14:24 +0900

    multerで生成されるファイルを使用後削除するよう修正

--- src/controller/storage/postStorageController.ts
+++ src/controller/storage/postStorageController.ts
@@ -1,36 +1,39 @@
 import { Request, Response, NextFunction } from "express";
 import { bucket, db } from "../../firebase";
-import { util } from "@google-cloud/common";
+import fs from 'fs';
 
 // storageに画像をアップロードする
 export function postStorageController(req: Request, res: Response, next: NextFunction) {
-    const files: Express.Multer.File[] = req.files['Files']
-    let uploadFilePath = ''
+    const files: Express.Multer.File[] = req.files['Files'];
+    const STORAGE_ROOT = "https://firebasestorage.googleapis.com/v0/b";
+    const bucketName = bucket.name;
     
-    files.forEach(fileInfo =>{
-        uploadFilePath = `files/${fileInfo.originalname}`
-        bucket.upload(fileInfo.path, {destination: uploadFilePath}, error => {
+    files.forEach(fileInfo => {
+        console.log(`fileInfo${JSON.stringify(fileInfo)}`)
+        const uploadFilePath = `files/${fileInfo.originalname}`
+        console.log(`uploadFilePath:${uploadFilePath}`)
+        bucket.upload(fileInfo.path, {destination: uploadFilePath, contentType: fileInfo.mimetype}, error => {
             if(error) {
                 console.log('failed storage post');
                 console.log(error.message);
-                res.sendStatus(404);
-                return;
+                return res.sendStatus(404);
             } else {
                 console.log('success storage post');
                 
                 // storageに画像をアップロードできた場合は、そのURLをrealtime databaseに保存する
-                const STORAGE_ROOT = "https://firebasestorage.googleapis.com/v0/b";
-                const bucketName = bucket.name;
                 const dlPath = encodeURIComponent(uploadFilePath);
                 const dlURL = `${STORAGE_ROOT}/${bucketName}/o/${dlPath}?alt=media`;
     
                 db.ref('files').push().set(dlURL , error => {
+                    console.log('called db.ref.push()')
                     if(error) {
-                        console.log('failed database fileURL post');
-                        res.sendStatus(404);
-                        return;
+                        console.log(`failed database ${uploadFilePath} post`);
+                        return res.sendStatus(404);
                     } else {
-                        console.log('success database fileURL post');
+                        console.log(`success database ${uploadFilePath} post`);
+
+                        // 一時的にサーバにおいたファイルを削除
+                        fs.unlinkSync(fileInfo.path)
                     }    
                 }
                 )
